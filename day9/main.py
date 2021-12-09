10d504dae3fbdc71500f4881ffff9946c76f106b
Kenji Yamashita <y.kenji.b@gmail.com>
2021-12-09 17:27:50 +0100

    day9 - clean code

--- day9/main.py
+++ day9/main.py
@@ -5,14 +5,12 @@ from typing import Any, Set, Tuple
 
 BIG_VAL = 9
 
-Heatmap = dict
-
 
 def get_local_file_abs_path(file_name: str) -> str:
     return os.path.join(os.path.dirname(__file__), file_name)
 
 
-def load_heatmap(input_path: str) -> Heatmap:
+def load_heatmap(input_path: str) -> dict:
     with open(input_path, "r") as file:
         heatmap = defaultdict(lambda: BIG_VAL)
         for y, line in enumerate(file.readlines()):
@@ -21,7 +19,7 @@ def load_heatmap(input_path: str) -> Heatmap:
     return heatmap
 
 
-def find_mins(heatmap: Heatmap) -> list[tuple[Any, Any]]:
+def find_mins(heatmap: dict) -> list[tuple[Any, Any]]:
     def is_min(x, y) -> bool:
         return (
             heatmap[x, y] < heatmap[x + 1, y]
@@ -33,7 +31,7 @@ def find_mins(heatmap: Heatmap) -> list[tuple[Any, Any]]:
     return list((x, y) for (x, y) in tuple(heatmap.keys()) if is_min(x, y))
 
 
-def visit(x: int, y: int, visited: Set[Tuple[int, int]], heatmap: Heatmap) -> Set[Tuple[int, int]]:
+def visit(x: int, y: int, visited: Set[Tuple[int, int]], heatmap: dict) -> Set[Tuple[int, int]]:
     visited.add((x, y))
     for i, j in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
         if (i, j) not in visited and heatmap[i, j] < 9:
@@ -41,7 +39,7 @@ def visit(x: int, y: int, visited: Set[Tuple[int, int]], heatmap: Heatmap) -> Se
     return visited
 
 
-def bassin_size(x: int, y: int, heatmap: Heatmap) -> int:
+def bassin_size(x: int, y: int, heatmap: dict) -> int:
     return len(visit(x, y, set(), heatmap))
 
 
