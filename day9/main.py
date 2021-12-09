9136ceb61865904445e5329f17c795b608d5e32a
Kenji Yamashita <y.kenji.b@gmail.com>
2021-12-09 16:53:33 +0100

    day9 - simplify with defaultdict

--- day9/main.py
+++ day9/main.py
@@ -1,10 +1,11 @@
-from functools import reduce
 import os
+from collections import defaultdict
+from functools import reduce
 from typing import List, NewType, Set, Tuple
 
 BIG_VAL = 10
 
-Heatmap = NewType("Heatmap", List[List[int]])
+Heatmap = NewType("Heatmap", dict)
 
 
 def get_local_file_abs_path(file_name: str) -> str:
@@ -13,32 +14,29 @@ def get_local_file_abs_path(file_name: str) -> str:
 
 def load_heatmap(input_path: str) -> Heatmap:
     with open(input_path, "r") as file:
-        heatmap = [
-            [BIG_VAL] + [int(c) for c in line.strip("\n")] + [BIG_VAL] for line in file.readlines()
-        ]
-    height = len(heatmap)
-    width = len(heatmap[0]) - 2
-    heatmap.insert(0, [BIG_VAL] * (width + 2))
-    heatmap.append([BIG_VAL] * (width + 2))
-    return heatmap, width, height
+        heatmap = defaultdict(lambda: BIG_VAL)
+        for y, line in enumerate(file.readlines()):
+            for x, c in enumerate(line.strip()):
+                heatmap[(x, y)] = int(c)
+    return heatmap
 
 
-def find_mins(heatmap: Heatmap, width: int, height: int) -> int:
+def find_mins(heatmap: Heatmap) -> int:
     def is_min(x, y) -> bool:
         return (
-            heatmap[y][x] < heatmap[y - 1][x]
-            and heatmap[y][x] < heatmap[y + 1][x]
-            and heatmap[y][x] < heatmap[y][x - 1]
-            and heatmap[y][x] < heatmap[y][x + 1]
+            heatmap[x, y] < heatmap[x + 1, y]
+            and heatmap[x, y] < heatmap[x - 1, y]
+            and heatmap[x, y] < heatmap[x, y + 1]
+            and heatmap[x, y] < heatmap[x, y - 1]
         )
 
-    return list((x, y) for y in range(1, height + 1) for x in range(1, width + 1) if is_min(x, y))
+    return list((x, y) for (x, y) in tuple(heatmap.keys()) if is_min(x, y))
 
 
 def visit(x: int, y: int, visited: Set[Tuple[int, int]], heatmap: Heatmap) -> Set[Tuple[int, int]]:
     visited.add((x, y))
     for i, j in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
-        if (i, j) not in visited and heatmap[j][i] < 9:
+        if (i, j) not in visited and heatmap[i, j] < 9:
             visited |= visit(i, j, visited, heatmap)
     return visited
 
@@ -48,13 +46,13 @@ def bassin_size(x: int, y: int, heatmap: Heatmap) -> int:
 
 
 def part_1(input_path: str) -> int:
-    heatmap, width, height = load_heatmap(input_path)
-    return sum(heatmap[y][x] + 1 for x, y in find_mins(heatmap, width, height))
+    heatmap = load_heatmap(input_path)
+    return sum(heatmap[x, y] + 1 for x, y in find_mins(heatmap))
 
 
 def part_2(input_path: str) -> int:
-    heatmap, width, height = load_heatmap(input_path)
-    mins = find_mins(heatmap, width, height)
+    heatmap = load_heatmap(input_path)
+    mins = find_mins(heatmap)
     bassin_sizes = [bassin_size(*pos, heatmap) for pos in mins]
     largest_bassins = list(reversed(sorted(bassin_sizes)))[:3]
     return reduce(lambda x, acc: x * acc, largest_bassins, 1)
