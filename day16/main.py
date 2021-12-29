d298f13ed9a8c5b0d5d3cb74d9bf412f81730549
Kenji Yamashita <y.kenji.b@gmail.com>
2021-12-29 16:18:52 +0100

    day16/part2 - solved

--- day16/main.py
+++ day16/main.py
@@ -1,5 +1,6 @@
 import os
 from functools import reduce
+from math import prod
 
 
 def get_local_file_abs_path(file_name: str) -> str:
@@ -11,15 +12,11 @@ def load_message(input_path: str) -> str:
         return file.readline().strip()
 
 
-def read_packet(binary_message: str) -> tuple[list, str]:
-    packets = []
+def read_packet(binary_message: str) -> tuple[tuple, str]:
     if int(binary_message):
         version = int(binary_message[:3], base=2)
         type_id = int(binary_message[3:6], base=2)
         binary_message = binary_message[6:]
-        print(f"{version=}")
-        print(f"{type_id=}")
-        print(f"{binary_message=}")
         match type_id:
             case 4:
                 content = ""
@@ -29,51 +26,77 @@ def read_packet(binary_message: str) -> tuple[list, str]:
                     if code == "0":
                         break
                 decoded = int(content, base=2)
-                packets.append((version, type_id, decoded))
+                return (version, type_id, decoded), binary_message
             case _:
                 length_type, binary_message = int(binary_message[0], base=2), binary_message[1:]
-                print(f"{length_type=}", binary_message)
                 match length_type:
                     case 0:
                         subpacket_length, binary_message = int(binary_message[0:15], base=2), binary_message[15:]
-                        print(f"{subpacket_length=}", binary_message)
-                        packets.append((version, type_id, subpacket_length))
                         subpackets = []
                         starting_len = len(binary_message)
-                        print(f"{len(binary_message)=}")
-                        print(f"{starting_len=}")
-                        print(f"{subpacket_length=}")
                         while len(binary_message) > (starting_len - subpacket_length):
                             if binary_message == "000":
                                 breakpoint()
                             subpacket, binary_message = read_packet(binary_message)
-                            packets.extend(subpacket)
+                            subpackets.append(subpacket)
+                        return (version, type_id, subpackets), binary_message
                     case 1:
                         packets_to_read, binary_message = int(binary_message[0:11], base=2), binary_message[11:]
-                        print(f"{packets_to_read=}", binary_message)
-                        packets.append((version, type_id, packets_to_read))
                         subpackets = []
                         for _ in range(packets_to_read):
                             subpacket, binary_message = read_packet(binary_message)
-                            packets.extend(subpacket)
+                            subpackets.append(subpacket)
+                        return (version, type_id, subpackets), binary_message
 
-    return packets, binary_message
+
+def flatten(packet: tuple) -> list:
+    output = []
+    if type(packet[2]) is list:
+        output.append((packet[0], packet[1], "list"))
+        for subpacket in packet[2]:
+            output.extend(flatten(subpacket))
+    else:
+        output.append(packet)
+    return output
+
+
+def process(packet: list) -> int:
+    match packet[1]:
+        case 4:
+            return packet[2]
+        case 0:
+            return sum(process(sub) for sub in packet[2])
+        case 1:
+            return prod(process(sub) for sub in packet[2])
+        case 2:
+            return min(process(sub) for sub in packet[2])
+        case 3:
+            return max(process(sub) for sub in packet[2])
+        case 5:
+            return 1 if process(packet[2][0]) > process(packet[2][1]) else 0
+        case 6:
+            return 1 if process(packet[2][0]) < process(packet[2][1]) else 0
+        case 7:
+            return 1 if process(packet[2][0]) == process(packet[2][1]) else 0
+    return 0
 
 
 def part_1(input_path: str) -> int:
     message = load_message(input_path)
-    # message = "A0016C880162017C3686B18A3D4780"
     end_length = len(message) * 4
     hex_as_binary = bin(int(message, base=16))
     binary_message = hex_as_binary[2:].zfill(end_length)
-    print(binary_message)
     packets, _ = read_packet(binary_message)
-    print(packets)
-    return reduce(lambda acc, e: e[0] + acc, packets, 0)
+    return reduce(lambda acc, e: e[0] + acc, flatten(packets), 0)
 
 
 def part_2(input_path: str) -> int:
-    return load_message(input_path)
+    message = load_message(input_path)
+    end_length = len(message) * 4
+    hex_as_binary = bin(int(message, base=16))
+    binary_message = hex_as_binary[2:].zfill(end_length)
+    packets, _ = read_packet(binary_message)
+    return process(packets)
 
 
 if __name__ == "__main__":
